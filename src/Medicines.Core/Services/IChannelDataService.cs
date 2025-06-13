711468a556237e50ed9a397bc9cd2cf482df9e52
qievenz <ivann96@gmail.com>
2025-06-13 13:23:41 -0300

    refactor: remove unused classes

--- src/Medicines.Core/Services/IChannelDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Medicines.Core.Services
-{
-    public interface IChannelDataService<T>
-    {
-        void CompletePublishing();
-        Task PublishMessageAsync(T message);
-        Task StartProcessingAsync(CancellationToken cancellationToken = default);
-    }
-}
